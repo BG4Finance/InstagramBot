ffab0e44005adf2ca8a093404d4bd32f02348494
G <54356938+BG4Finance@users.noreply.github.com>
2020-03-09 13:16:45 +0100

    Update ig_bot.py

--- ig_bot.py
+++ ig_bot.py
@@ -17,18 +17,25 @@ class igbot:
 		self.pw = pw
 		print(user)
 		self.driver=webdriver.Chrome("/Users/Barbarossa/Desktop/Prog/bots/chromedriver")
-		time.sleep(1)
+		self.driver.implicitly_wait(10)
 		self.driver.get('https://www.instagram.com/accounts/login/')
-		time.sleep(1)
+		self.driver.implicitly_wait(10)
 		self.driver.find_element_by_name('username').send_keys(user)
 		self.driver.find_element_by_name('password').send_keys(pw)
 		self.driver.find_element_by_xpath('/html/body/div[1]/section/main/div/article/div/div[1]/div/form/div[4]/button/div').click()
-		time.sleep(3.5)
+		self.driver.implicitly_wait(10)
 		#Find that annoying notification button and say no!
 		self.driver.find_element_by_xpath("//button[contains(text(), 'Not Now')]").click()
 		time.sleep(uniform(1,5))
 		#database of the following people of each account
-		self.dbname='{}_following_list.csv'.format(user)
+		self.dbname = '{}_following_list.csv'.format(user)
+		try:
+			self.following_list= pd.read_csv(self.dbname,delimiter=',').iloc[:,1]
+			print('Existing User')
+		except FileNotFoundError as e:
+			file_status = 'missing'
+			self.following_list = pd.DataFrame(columns=['users'])
+			print('New User Onboarding')
 		#Now we are in the IG homepage, let's move around
 
 	def randomise(self):
@@ -46,41 +53,64 @@ class igbot:
 		self.driver.close()
 
 	def newfollow(self,hashtag_list=['instagram']):
-		try:
-			self.following_list= pd.read_csv(self.dbname,delimiter=',').iloc[:,1]
-			print('Existing User')
-		except FileNotFoundError as e:
-			file_status = 'missing'
-			self.following_list = pd.DataFrame(columns=['users'])
-			print('New User Onboarding')
 		new_followed = []
 		tag = 0
 		followed = 0
 		likes = 0
 		comments = 0
 		#10 Comments
-		comm = ['Wow, amazing!',"Aaahw, that's something", "Damn!", "Looove it!", "Hey where u from?", "Yoooo", "That's something else...","I would like to ride it...", "I kind like it", "Hey, where u at?"]
+		comm = ['Wow, amazing! you should check out @hylus__ ',"Aaahw, that's something! You should check out @hylus__ ", "Damn! S/O to @hylus__ ", "Looove it! go check @hylus__ ", "Hey where u from? Do you know about @hylus__ ", "Yoooo, @hylus__ is out there ", "That's something else... like @hylus__ ","I would like to ride it like i ride my @hylus__ ", "I kind like it, but @hylus__ is way better", "Hey, where u at? Do you know about @hylus__ ? "]
 
 		for tag in hashtag_list:
 			#Go to explore hashtag page
 			self.driver.get('https://www.instagram.com/explore/tags/'+tag+'/')
 			time.sleep(1)
-			#Open first foto
-			self.thumb = self.driver.find_element_by_xpath('//*[@id="react-root"]/section/main/article/div[1]/div/div/div[1]/div[1]/a/div')
+			#Open a random foto foto out of first 9
+			self.thumb = self.driver.find_element_by_xpath('//*[@id="react-root"]/section/main/article/div[1]/div/div/div[{0}]/div[{1}]/a/div'.format(randint(1,3),randint(1,3)))
 			self.thumb.click()
 			time.sleep(2)
 			try:
 				for x in range(1,60):
 					#Identifiy the user
-					time.sleep(1)
-					username = self.driver.find_element_by_css_selector('body > div._2dDPU.vCf6V > div.zZYga > div > article > header > div.o-MQd.z8cbW > div.PQo_0.RqtMr > div.e1e1d > a').text
+					self.driver.implicitly_wait(10)
+					username = self.driver.find_element_by_xpath('/html/body/div[4]/div[2]/div/article/header/div[2]/div[1]/div[1]/a').text
 					#Interactions start here!
+					#Should I comment?
+					if randint(0,100) > 45:
+						time.sleep(uniform(2,5))
+						self.driver.find_element_by_xpath('/html/body/div[4]/div[2]/div/article/div[2]/section[3]/div[1]/form/textarea').click()
+						self.comment_box = self.driver.find_element_by_xpath('/html/body/div[4]/div[2]/div/article/div[2]/section[3]/div[1]/form/textarea')
+						self.comment_box.send_keys(comm[randint(0,9)])
+						time.sleep(1)
+						self.driver.find_element_by_xpath('/html/body/div[4]/div[2]/div/article/div[2]/section[3]/div/form/button').click()
+						self.driver.implicitly_wait(10)
+						#if self.driver.find_element_by_xpath("/html/body/div[5]/div/div/div[1]/h3").text == "Action Blocked":
+						#	self.driver.find_element_by_css_selector("body > div.RnEpo.Yx5HN > div > div > div.mt3GC > button.aOOlW.HoLwm").click()
+						#else:
+						#	comments += 1
+						comments += 1
+						print(username + ' commented correctly NF')
+						time.sleep(uniform(1,3))
+
+					#Should I like?
+					if randint(0,100) > 99:
+						time.sleep(uniform(0.5,2))
+						self.driver.find_element_by_css_selector('body > div._2dDPU.CkGkG > div.zZYga > div > article > div.eo2As > section.ltpMr.Slqrh > span.fr66n > button > svg').click()
+						#time.sleep(1)
+						#if self.driver.find_element_by_xpath("/html/body/div[5]/div/div/div[1]/h3").text == "Action Blocked":
+						#	self.driver.find_element_by_css_selector("body > div.RnEpo.Yx5HN > div > div > div.mt3GC > button.aOOlW.HoLwm").click()
+						#else:
+						#	likes += 1
+						likes += 1
+						print(username + ' liked correctly NF')
+						time.sleep(uniform(0.5,2))
+
 					#If account does not follow the user...
-					if self.driver.find_element_by_css_selector("body > div._2dDPU.vCf6V > div.zZYga > div > article > header > div.o-MQd > div.PQo_0 > div.bY2yH > button").text == "Follow":
+					if self.driver.find_element_by_xpath("/html/body/div[4]/div[2]/div/article/header/div[2]/div[1]/div[2]/button").text == "Follow":
 						#Should I follow?
 						if randint(0,100) > 75:
 							time.sleep(uniform(2,3))
-							self.driver.find_element_by_css_selector("body > div._2dDPU.vCf6V > div.zZYga > div > article > header > div.o-MQd > div.PQo_0 > div.bY2yH > button").click()
+							self.driver.find_element_by_xpath("/html/body/div[4]/div[2]/div/article/header/div[2]/div[1]/div[2]/button").click()
 							#time.sleep(1)
 							#if self.driver.find_element_by_xpath("/html/body/div[5]/div/div/div[1]/h3").text == "Action Blocked":
 							#	self.driver.find_element_by_css_selector("body > div.RnEpo.Yx5HN > div > div > div.mt3GC > button.aOOlW.HoLwm").click()
@@ -100,68 +130,14 @@ class igbot:
 							updated_user_df.to_csv(self.dbname)
 						time.sleep(uniform(0.5,3))
 
-						#Should I comment?
-						if randint(0,100) > 76:
-							time.sleep(uniform(2,5))
-							self.driver.find_element_by_css_selector('body > div._2dDPU.vCf6V > div.zZYga > div > article > div.eo2As > section.sH9wk._JgwE > div > form > textarea').click()
-							self.comment_box = self.driver.find_element_by_css_selector('body > div._2dDPU.vCf6V > div.zZYga > div > article > div.eo2As > section.sH9wk._JgwE > div > form > textarea')
-							self.comment_box.send_keys(comm[randint(0,9)])
-							self.comment_box.send_keys(Keys.ENTER)
-							#time.sleep(1)
-							#if self.driver.find_element_by_xpath("/html/body/div[5]/div/div/div[1]/h3").text == "Action Blocked":
-							#	self.driver.find_element_by_css_selector("body > div.RnEpo.Yx5HN > div > div > div.mt3GC > button.aOOlW.HoLwm").click()
-							#else:
-							#	comments += 1
-							comments += 1
-							print(username + ' commented correctly NF')
-							time.sleep(uniform(0.5,2))
-
-						#Should I like?
-						if randint(0,100) > 69:
-							time.sleep(uniform(0.5,2))
-							self.driver.find_element_by_css_selector('body > div._2dDPU.vCf6V > div.zZYga > div > article > div.eo2As > section.ltpMr.Slqrh > span.fr66n > button > svg').click()
-							#time.sleep(1)
-							#if self.driver.find_element_by_xpath("/html/body/div[5]/div/div/div[1]/h3").text == "Action Blocked":
-							#	self.driver.find_element_by_css_selector("body > div.RnEpo.Yx5HN > div > div > div.mt3GC > button.aOOlW.HoLwm").click()
-							#else:
-							#	likes += 1
-							likes += 1
-							print(username + ' liked correctly NF')
-							time.sleep(uniform(0.5,2))
-
-					#If the user is not followed?
 					else:
-						if randint(0,100) > 60:
-							time.sleep(uniform(0.5,2))
-							self.driver.find_element_by_css_selector('body > div._2dDPU.vCf6V > div.zZYga > div > article > div.eo2As > section.ltpMr.Slqrh > span.fr66n > button > svg').click()
-							#time.sleep(1)
-							#if self.driver.find_element_by_xpath("/html/body/div[5]/div/div/div[1]/h3").text == "Action Blocked":
-							#	self.driver.find_element_by_css_selector("body > div.RnEpo.Yx5HN > div > div > div.mt3GC > button.aOOlW.HoLwm").click()
-							#else:
-							#	likes += 1
-							likes += 1
-							print(username + ' Liked correctly AF')
-							time.sleep(uniform(0.5,2))
-
-						if randint(0,100) > 75:
-							time.sleep(uniform(2,5))
-							self.driver.find_element_by_css_selector('body > div._2dDPU.vCf6V > div.zZYga > div > article > div.eo2As > section.sH9wk._JgwE > div > form > textarea').click()
-							self.comment_box = self.driver.find_element_by_css_selector('body > div._2dDPU.vCf6V > div.zZYga > div > article > div.eo2As > section.sH9wk._JgwE > div > form > textarea')
-							self.comment_box.send_keys(comm[randint(0,9)])
-							self.comment_box.send_keys(Keys.ENTER)
-							#time.sleep(1)
-							#if self.driver.find_element_by_xpath("/html/body/div[5]/div/div/div[1]/h3").text == "Action Blocked":
-							#	self.driver.find_element_by_css_selector("body > div.RnEpo.Yx5HN > div > div > div.mt3GC > button.aOOlW.HoLwm").click()
-							#else:
-							#	comments += 1
-							comments+= 1
-							print(username + ' commented correctly AF')
-							time.sleep(uniform(0.5,2))
+						print('{} already followed'.format(username))
 
 					time.sleep(uniform(1,2))
 					for i in range(1,randint(2,10)):
-						self.driver.find_element_by_css_selector('body > div._2dDPU.vCf6V > div.EfHg9 > div > div > a._65Bje.coreSpriteRightPaginationArrow').click()
-						time.sleep(uniform(2,3))
+						self.driver.implicitly_wait(10)
+						self.driver.find_element_by_css_selector("body > div._2dDPU.CkGkG > div.EfHg9 > div > div > a._65Bje.coreSpriteRightPaginationArrow").click()
+						time.sleep(uniform(0.5,3))
 
 			except Exception as e:
 				print('Error on line {}'.format(sys.exc_info()[-1].tb_lineno), type(e).__name__, e)
@@ -174,13 +150,6 @@ class igbot:
 		print('supmate')
 
 	def unfollow(self):
-		try:
-			self.following_list= pd.read_csv(self.dbname,delimiter=',').iloc[:,1]
-			print('Existing User')
-		except FileNotFoundError as e:
-			file_status = 'missing'
-			self.following_list = pd.DataFrame(columns=['users'])
-			print('New User Onboarding')
 		for times in range(1,5):
 			# Open profile page through profile pick
 			self.driver.get('https://www.instagram.com/{}/'.format(self.user))
@@ -194,6 +163,5 @@ class igbot:
 				time.sleep(0.5)
 				self.driver.find_element_by_xpath('/html/body/div[5]/div/div/div[3]/button[1]').click()
 
-
-#Please define an hashtag_list
+hashtag_list = ['art','skateboard','tonyhawk', 'wall', 'design', 'board']
 igbot(account[ale], password[ale]).newfollow(hashtag_list)
